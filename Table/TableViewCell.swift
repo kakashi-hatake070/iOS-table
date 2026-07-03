3febafd133110d0e3102111da4bfe3fa5aec11de
kunal sharma <sharmakunal7184@gmail.com>
2026-07-03 20:32:16 +0530

    First commit

---
+++ Table/TableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewCell.swift
+//  Table
+//
+//  Created by Apple System 22 on 27/06/26.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var Naruto: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+        
+        
+    }
+    
+}
--- Table/ViewController.swift
+++ Table/ViewController.swift
@@ -7,13 +7,92 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stud.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell")
+                as? TableViewCell else {return UITableViewCell()}
+        cell.Naruto.text = stud[indexPath.row]
+        return cell
+    }
+    
 
+    
+    
+    @IBOutlet weak var outlet: UITableView!
+    
+    var stud: [String] = ["Amit", "Rahul", "Priya", "Sneha", "Rahul", "Priya", "Sneha", "Rahul", "Priya", "Sneha", "Rahul", "Priya", "Sneha", "Rahul", "Priya", "Sneha", "Rahul", "Priya", "Sneha"]
+    
+ 
+    
+    
+           
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        outlet.delegate = self
+        outlet.dataSource = self
+        outlet.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
+        
+        
+        
+        
+        
+        
     }
-
-
 }
+       
+      
+        
+        
+        
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        var names = ["Amit", "Rahul", "Priya", "Sneha"]
+//
+//       
+//        names.append("Vikram")
+//
+//       
+//        print(names)
+       
+        
+        
+    
+
+
+
 
